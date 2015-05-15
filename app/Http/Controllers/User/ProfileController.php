5b98384f88bf1ecd5b1a1757bf46689ef5752c28
Bertrand Kintanar <bertrand.kintanar@gmail.com>
2015-05-15 18:35:31 +0800

    Added Sentry Middleware

--- app/Http/Controllers/User/ProfileController.php
+++ app/Http/Controllers/User/ProfileController.php
@@ -1,12 +1,36 @@
 <?php namespace App\Http\Controllers\User;
 
-use App\Eloquent\SocialUser;
 use App\Http\Controllers\Controller;
 
 /**
- * Class AuthController
+ * Class ProfileController
  * @package App\Http\Controllers\User
+ *
+ * @Middleware("auth")
  */
 class ProfileController extends Controller {
 
+    /**
+     * Show
+     * @Get("u/{username}/edit")
+     */
+    public function edit($username)
+    {
+
+    }
+
+    /**
+     * @Get("u/{username}")
+     *
+     * @param $username
+     *
+     * @return \Illuminate\View\View
+     */
+    public function show($username)
+    {
+        dd('test');
+        $this->data['page_title'] = 'Upload Photo';
+
+        return $this->template('user.upload-photo');
+    }
 }

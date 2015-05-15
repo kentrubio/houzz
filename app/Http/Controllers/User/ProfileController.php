c97889bc0c746c83321c51db578d99db28b11d1d
Bertrand Kintanar <bertrand.kintanar@gmail.com>
2015-05-15 18:06:15 +0800

    Initial User Controllers

---
+++ app/Http/Controllers/User/ProfileController.php
@@ -0,0 +1,12 @@
+<?php namespace App\Http\Controllers\User;
+
+use App\Eloquent\SocialUser;
+use App\Http\Controllers\Controller;
+
+/**
+ * Class AuthController
+ * @package App\Http\Controllers\User
+ */
+class ProfileController extends Controller {
+
+}
